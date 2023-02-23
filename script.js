9d89ab5e214b291f48f4bede0f87bc6e99619f32
j-winston <kenjameswinston@gmail.com>
2023-02-22 19:50:28 -0500

    create matrixes for tic-tac-toe console

--- script.js
+++ script.js
@@ -8,6 +8,40 @@ const gameBoard = (() => {
     tie: false,
   };
 
+  const _checkWinner = () => {
+    // Check along each of the 3 columns for a vertical match
+    for (let i = 0; i < _gameArray.length; i++) {
+      const token = _gameArray[i][0];
+      if (token != "*") {
+        if (_gameArray[i][1] === token) {
+          if (_gameArray[i][2] === token) {
+            _currentState.winner = token;
+          }
+        }
+      }
+    }
+
+    // Check along each of the rows
+    // for (let i = 0; i < _gameArray.length; i++) {
+    //   const token = _gameArray[i][i+1];
+    //   if (_gameArray[1][i] === token) {
+    //     if (_gameArray[2][i] === token) {
+    //       _currentState.winner = token;
+    //     }
+    //   }
+    // }
+  };
+
+  const gameState = () => {
+    if (_currentState.tie) {
+      return "Tie";
+    }
+
+    if (_currentState.winner) {
+      return 'The winner is ' + _currentState.winner;
+    }
+  };
+
   // Create a 3x3 array to store values.
   // Later we will write this to UI
   const _intializeArray = () => {
@@ -17,9 +51,13 @@ const gameBoard = (() => {
 
     for (let i = 0; i < _gameArray.length; i++) {
       for (let j = 0; j < _gameArray.length; j++) {
-        _gameArray[i][j] = 'O'; 
+        _gameArray[i][j] = "*";
       }
     }
+  };
+
+  const drawBoard = () => {
+    _intializeArray();
   };
 
   const insert = (positionMarker) => {
@@ -27,23 +65,21 @@ const gameBoard = (() => {
     const row = positionMarker.row;
     const token = positionMarker.token;
 
-      _gameArray[col][row] = token;
-  };
-
-  const drawBoard = () => {
-    _intializeArray();
+    //switch this around!
+    _gameArray[col][row] = token;
+      _checkWinner();
   };
 
   const printBoard = () => {
     for (let i = 0; i < _gameArray.length; i++) {
       for (let j = 0; j < _gameArray.length; j++) {
+        process.stdout.write(_gameArray[j][i]);
       }
-        console.log(_gameArray[i])
+      console.log();
     }
-
   };
 
-  return { insert, drawBoard, printBoard };
+  return { insert, drawBoard, printBoard, gameState };
 })();
 
 // Player logic
@@ -66,8 +102,11 @@ const player = (name, token) => {
 // Main game loop
 
 gameBoard.drawBoard();
-
-const player1 = player("james", "X");
+const player1 = player("james", "O"); // Get player input
 
 gameBoard.insert(player1.markPos(0, 0));
+gameBoard.insert(player1.markPos(0, 1));
+gameBoard.insert(player1.markPos(0, 2));
+
 gameBoard.printBoard();
+console.log(gameBoard.gameState());
